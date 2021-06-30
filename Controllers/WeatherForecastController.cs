5a7577c56ae42431e74d4ab0c855b48ed9bda8b2
Sylk <sy1lk@vn.ibm.com>
2021-06-30 19:19:29 +0700

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace counter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static int Counter = 0;
+
+        [HttpGet]
+        public int Get()
+        {
+            return Counter;
+        }
+
+        [HttpPost("decrease")]
+        public void Decrease(int amount)
+        {
+            Counter = Counter - amount;
+        }
+
+        [HttpPost("increase")]
+        public void Increase(int amount)
+        {
+            Counter = Counter + amount;
+        }
+    }
+}
